5dba75e4d6e24c88591cdf9452fc9c63d6848798
Aaron Alejandro Martínez Solis <aaronjrale@gmail.com>
2024-08-04 19:09:14 -0600

    [UPD]-COSILLAS

--- app/Http/Controllers/DibujarController.php
+++ app/Http/Controllers/DibujarController.php
@@ -11,6 +11,8 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
+
 
 class DibujarController extends Controller
 {
